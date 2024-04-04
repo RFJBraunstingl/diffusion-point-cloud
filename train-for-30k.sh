07108bb4b547f11617d0259be83384bcd54361e9
User <rbraunstingl@martin.cvl.tuwien.ac.at>
2024-04-04 22:36:22 +0200

    add script to train different generators

---
+++ train-for-30k.sh
@@ -0,0 +1,14 @@
+python train_gen.py --categories bookshelf --device cuda:3 --max_iters 32000
+python train_gen.py --categories bottle --device cuda:3 --max_iters 32000
+python train_gen.py --categories bowl --device cuda:3 --max_iters 32000
+python train_gen.py --categories car --device cuda:3 --max_iters 32000
+python train_gen.py --categories chair --device cuda:3 --max_iters 32000
+python train_gen.py --categories guitar --device cuda:3 --max_iters 32000
+python train_gen.py --categories keyboard --device cuda:3 --max_iters 32000
+python train_gen.py --categories lamp --device cuda:3 --max_iters 32000
+python train_gen.py --categories laptop --device cuda:3 --max_iters 32000
+python train_gen.py --categories monitor --device cuda:3 --max_iters 32000
+python train_gen.py --categories piano --device cuda:3 --max_iters 32000
+python train_gen.py --categories sofa --device cuda:3 --max_iters 32000
+python train_gen.py --categories table --device cuda:3 --max_iters 32000
+
