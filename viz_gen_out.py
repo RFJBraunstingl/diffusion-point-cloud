74285a16796eaee0e5d382aa676349eca538fcb5
Roman Braunstingl <rfj.braunstingl@gmail.com>
2024-04-05 22:24:34 +0200

    cleanup viz script

--- viz_gen_out.py
+++ viz_gen_out.py
@@ -11,10 +11,6 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--file', type=str, default='./results/viz/npy/out.npy')
 parser.add_argument('--save_dir', type=str, default='./results')
-parser.add_argument('--dataset_path', type=str, default='./data/shapenet.hdf5')
-parser.add_argument('--categories', type=str_list, default=['airplane'])
-parser.add_argument('--normalize', type=str, default='shape_bbox', choices=[None, 'shape_unit', 'shape_bbox'])
-parser.add_argument('--batch_size', type=int, default=128)
 
 args = parser.parse_args()
 pc_name = args.file.split("/")[-1].replace(".npy", "")
